7f49a46fc7b3b97571c578b6f9040fd00c4c78fb
leeyeonji28 <yeon102800@gmail.com>
2022-09-14 13:08:55 +0900

    세팅

---
+++ src/main/java/com/thanksto/backthanksto/BackThanksToApplication.java
@@ -0,0 +1,13 @@
+package com.thanksto.backthanksto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackThanksToApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackThanksToApplication.class, args);
+    }
+
+}
