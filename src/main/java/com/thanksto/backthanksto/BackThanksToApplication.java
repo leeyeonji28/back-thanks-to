7458d99ca6891799378eb211a616ef0f2835d38b
leeyeonji28 <yeon102800@gmail.com>
2022-09-16 02:00:49 +0900

    회원가입 로직 추가

--- src/main/java/com/thanksto/backthanksto/BackThanksToApplication.java
+++ src/main/java/com/thanksto/backthanksto/BackThanksToApplication.java
@@ -2,9 +2,16 @@ package com.thanksto.backthanksto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class BackThanksToApplication {
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(BackThanksToApplication.class, args);
--- src/main/java/com/thanksto/backthanksto/controller/ResApiController.java
+++ src/main/java/com/thanksto/backthanksto/controller/ResApiController.java
@@ -1,11 +1,20 @@
 package com.thanksto.backthanksto.controller;
 
+import com.thanksto.backthanksto.dao.UserRepository;
+import com.thanksto.backthanksto.domain.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class ResApiController {
+    private final UserRepository userRepository;
+    private final  BCryptPasswordEncoder bCryptPasswordEncoder;
+
     @GetMapping("/home")
     public String home() {
         return "home";
@@ -14,5 +23,13 @@ public class ResApiController {
     @PostMapping("/token")
     public String token() {
         return "token";
+    }
+
+    @PostMapping("/join")
+    public String join(@RequestBody User user) {
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        user.setRoles("ROLE_USER");
+        userRepository.save(user);
+        return "회원가입 완료";
     }
 }
