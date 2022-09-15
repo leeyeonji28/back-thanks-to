e349a1cd620b02e112967545a66dd5c22d9722a8
leeyeonji28 <yeon102800@gmail.com>
2022-09-15 12:14:03 +0900

    Filter 수정

--- src/main/java/com/thanksto/backthanksto/config/SecurityConfig.java
+++ src/main/java/com/thanksto/backthanksto/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.thanksto.backthanksto.config;
 
-import com.thanksto.backthanksto.filter.SecurityFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,8 +7,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.logout.LogoutFilter;
-import org.springframework.security.web.context.SecurityContextHolderFilter;
 
 @Configuration
 @EnableWebSecurity // 스프링 시큐리티 필터가 스프링 필터체인에 등록 됨
@@ -19,12 +16,11 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         return http
-                .addFilterBefore(new SecurityFilter(), SecurityContextHolderFilter.class)
+                //.addFilterBefore(new SecurityFilter(), SecurityContextHolderFilter.class) //SecurityContextHolderFilter가 실행되기 전에 걸어주는 것
                 .csrf().disable()
                 .cors()
                 .and()
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)//세션을 사용하지 않음
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)//세션을 사용하지 않음
                 .and()
                 .formLogin().disable()// jwt 인증방식을 위해 id pw를 폼로그인으로 처리를 안함
                 .httpBasic().disable()// http 기본 인증방식을 사용안함
@@ -32,7 +28,7 @@ public class SecurityConfig {
                         .antMatchers("/api/v1/user/**").access("hasRole('ROLE_USER') or hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')") // 해당 권한이 있는 사람만 접속 가능
                         .antMatchers("/api/v1/manager/**").access("hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
                         .antMatchers("/api/v1/admin/**").access("hasRole('ROLE_ADMIN')")
-                        .anyRequest().permitAll() // 위의 요청을 제외한 다른 요청은 전부 보안 요구 없이 요청을 허용
+                        .anyRequest().permitAll() // 위의 요청을 제외한 다른 요청은 전부 권한 없이 요청을 허용
                 )
                 .build();
     }
--- src/main/java/com/thanksto/backthanksto/config/WebMvcConfig.java
+++ src/main/java/com/thanksto/backthanksto/config/WebMvcConfig.java
@@ -10,8 +10,8 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/api/**") // 해당 하위 주소는 다 허용하겠다.
-                .allowedOrigins("*")
-                .allowedMethods("*")
-                .allowedHeaders("*");
+                .allowedOrigins("*") // 모든 ip에 응답을 허용
+                .allowedMethods("*") // 모든 post, get, put, delete, patch 요청 허용
+                .allowedHeaders("*"); // 모든 header에 응답을 허용
     }
 }
