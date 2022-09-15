980bc395fb22c5a32c35b880229835575084560e
leeyeonji28 <yeon102800@gmail.com>
2022-09-15 12:37:42 +0900

    임시 토큰 테스트

--- src/main/java/com/thanksto/backthanksto/config/FilterConfig.java
+++ src/main/java/com/thanksto/backthanksto/config/FilterConfig.java
@@ -1,17 +1,12 @@
 package com.thanksto.backthanksto.config;
 
-import com.thanksto.backthanksto.filter.SecurityFilter;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
+//@Configuration
 public class FilterConfig {
-    @Bean
-    public FilterRegistrationBean<SecurityFilter> filter1(){
-        FilterRegistrationBean<SecurityFilter> bean = new FilterRegistrationBean<>(new SecurityFilter());
-        bean.addUrlPatterns("/*");
-        bean.setOrder(0); // 낮은 번호가 필터 중에서 가장 먼저 실행 됨
-        return bean;
-    }
+//    @Bean
+//    public FilterRegistrationBean<SecurityFilter> filter1(){
+//        FilterRegistrationBean<SecurityFilter> bean = new FilterRegistrationBean<>(new SecurityFilter());
+//        bean.addUrlPatterns("/*");
+//        bean.setOrder(0); // 낮은 번호가 필터 중에서 가장 먼저 실행 됨
+//        return bean;
+//    }
 }
--- src/main/java/com/thanksto/backthanksto/filter/SecurityFilter.java
+++ src/main/java/com/thanksto/backthanksto/filter/SecurityFilter.java
@@ -1,16 +1,37 @@
 package com.thanksto.backthanksto.filter;
 
 import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class SecurityFilter implements Filter {
     //    필수 메소드
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        System.out.println("filter1");
-        chain.doFilter(request, response); // 끝내지 말고 계속 진행하기 위해 추가
 
-    // PrintWriter out = response.getWriter();
-    // out.print("hi");
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse res = (HttpServletResponse) response;
+
+        // 토근 : cos라고 가정
+        // id, pw가 정상적으로 들어와 로그인이 완료되면 토큰을 만들어주고 그걸 응답한다.
+        // 요청할 때마다 header에 Authorization에 value값으로 토큰을 가지고 오는데
+        // 그때 토큰이 넘어오면 이 토큰이 내가 만든 토큰이 맞는지 검증한다.
+        if (req.getMethod().equals("POST")) {
+            System.out.println("POST 요청됨");
+            String headerAuth = req.getHeader("Authorization");
+            System.out.println(headerAuth);
+
+            if (headerAuth.equals("cos")) {
+                chain.doFilter(req, res); // 끝내지 말고 계속 진행하기 위해 추가
+            } else {
+                PrintWriter out = res.getWriter();
+                out.println("not");
+            }
+        }
+
+
+
     }
 }
