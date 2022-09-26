0e944d95ac93b437f94bbfb0953e630455e06b57
leeyeonji28 <yeon102800@gmail.com>
2022-09-26 10:41:21 +0900

    post 단건 조회

--- src/main/java/com/thanksto/backthanksto/post/controller/PostController.java
+++ src/main/java/com/thanksto/backthanksto/post/controller/PostController.java
@@ -4,6 +4,8 @@ import com.thanksto.backthanksto.post.domain.Post;
 import com.thanksto.backthanksto.post.sevice.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -15,8 +17,16 @@ public class PostController {
     private final PostService postService;
 
     // post 전체 조회
-    @GetMapping("/list/all")
+    @GetMapping("/post/list/all")
     public List<Post> getPostList() {
         return this.postService.getPostList();
     }
+
+    // post 단건 조회
+    @GetMapping("/post/detail/{postId}")
+    public Post getPostDetail(@PathVariable Long postId){
+        return this.postService.getPostDetail(postId);
+    }
+
+
 }
--- src/main/java/com/thanksto/backthanksto/post/sevice/PostService.java
+++ src/main/java/com/thanksto/backthanksto/post/sevice/PostService.java
@@ -1,11 +1,13 @@
 package com.thanksto.backthanksto.post.sevice;
 
+import com.thanksto.backthanksto.post.DataNotFoundException;
 import com.thanksto.backthanksto.post.dao.PostRepository;
 import com.thanksto.backthanksto.post.domain.Post;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -14,5 +16,14 @@ public class PostService {
 
     public List<Post> getPostList() {
         return this.postRepository.findAll();
+    }
+
+    public Post getPostDetail(Long postId) {
+        Optional<Post> post = this.postRepository.findById(postId);
+        if (post.isPresent()){
+            return post.get();
+        } else {
+            throw new DataNotFoundException("Post not found");
+        }
     }
 }
